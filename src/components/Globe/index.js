906201ecbc91b4b78a5ac0042d56cb87cdf2c98c
Abhilash Singh <daft300punk@gmail.com>
2017-05-22 02:35:29 +0530

    Working heroku deploy from dist

---
+++ src/components/Globe/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Globe extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="globe-container"/>
+    );
+  }
+}
+
+export default Globe;
