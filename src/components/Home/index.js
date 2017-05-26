bc5b4931f43351704e8923cc65794f1ac2d9f02f
Abhilash Singh <daft300punk@gmail.com>
2017-05-27 03:39:03 +0530

    Welcome component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Globe from '../Globe';
+import Welcome from '../Welcome';
 
-const Home = () => {
-  return (
-    <div>
-      <Globe />
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showWelcome: true
+    };
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    setTimeout(() => {
+      this.setState({
+        showWelcome: false
+      });
+    }, 2500)
+  }
+
+  render() {
+    return(
+      <div>
+        <Globe />
+        {this.state.showWelcome && <Welcome />}
+      </div>
+    );
+  }
+}
+
 
 export default Home;
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1>TOPCODER STATS</h1>
+    </div>
+  );
+};
+
+export default Welcome;
