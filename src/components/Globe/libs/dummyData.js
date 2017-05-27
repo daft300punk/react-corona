5ca690d89a5a915f8af5759624c0112d421f4f17
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 01:30:02 +0530

    Update dummy data

--- src/components/Globe/libs/dummyData.js
+++ src/components/Globe/libs/dummyData.js
@@ -1,1 +1,15 @@
 export default [["1990",[6,159,0.001, 24, 144, 1, 44, 100, 1, 64, 144, 1]]];
+
+export const dataFlat = [
+  {lat: 6, long: 159, type: 'des', info: {name: 'comp1'}},
+  {lat: 24, long: 144, type: 'dev', info: {name: 'comp2'}},
+  {lat: 44, long: 100, type: 'dsc', info: {name: 'comp3'}},
+  {lat: 64, long: 144, type: 'des', info: {name: 'comp4'}},
+];
+
+export const dataMagnitude = [
+  {lat: 6, long: 159, type: 'des', info: {name: 'comp1'}, magnitude: .005},
+  {lat: 24, long: 144, type: 'dev', info: {name: 'comp2'}, magnitude: .015},
+  {lat: 44, long: 100, type: 'dsc', info: {name: 'comp3'}, magnitude: .205},
+  {lat: 64, long: 144, type: 'des', info: {name: 'comp4'}, magnitude: .105},
+];
