afab2d3e45bf42e99533c7bf5eaf4b4790fca707
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 01:29:44 +0530

    Accept data dynamically

--- src/components/Globe/libs/initGlobe.js
+++ src/components/Globe/libs/initGlobe.js
@@ -1,16 +1,21 @@
 import DAT from './globe';
 
-import data from './dummyData';
+import {dataFlat, dataMagnitude} from './dummyData';
 
 export default function initGlobe() {
   const container = document.getElementById('container');
-  const globe = new DAT.Globe(container);
+  let globe;
 
-  window.data = data;
+  window.data = dataFlat;
+  const sizeOfPoint = 2;
 
-  for(let i = 0; i < data.length; i++) {
-    globe.addData(data[i][1], { format: 'magnitude', name: data[i][0], animated: false });
+  if(dataMagnitude[0].magnitude) {
+    globe = new DAT.Globe(container, 'magnitude', sizeOfPoint);
+  } else {
+    globe = new DAT.Globe(container, 'flat', sizeOfPoint);
   }
+  globe.addData(dataMagnitude);
+
   globe.createPoints();
   globe.animate();
 }
