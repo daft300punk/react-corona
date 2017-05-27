5ce2fe9d537b11234979c0c8c703fde5ba27d8cf
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 03:02:40 +0530

    Generate dummy data randomly

--- src/components/Globe/libs/dummyData.js
+++ src/components/Globe/libs/dummyData.js
@@ -1,15 +1,57 @@
-export default [["1990",[6,159,0.001, 24, 144, 1, 44, 100, 1, 64, 144, 1]]];
+const types = ['dev', 'dsc', 'des'];
 
-export const dataFlat = [
-  {lat: 6, long: 159, type: 'des', info: {name: 'comp1'}},
-  {lat: 24, long: 144, type: 'dev', info: {name: 'comp2'}},
-  {lat: 44, long: 100, type: 'dsc', info: {name: 'comp3'}},
-  {lat: 64, long: 144, type: 'des', info: {name: 'comp4'}},
-];
+const makeDummyDataFlat = () => {
+  let dataFlat = [];
+  for(let i = 0; i < 1000; i++) {
+    let multiplier1, multiplier2;
 
-export const dataMagnitude = [
-  {lat: 6, long: 159, type: 'des', info: {name: 'comp1'}, magnitude: .005},
-  {lat: 24, long: 144, type: 'dev', info: {name: 'comp2'}, magnitude: .015},
-  {lat: 44, long: 100, type: 'dsc', info: {name: 'comp3'}, magnitude: .205},
-  {lat: 64, long: 144, type: 'des', info: {name: 'comp4'}, magnitude: .105},
-];
+    if( Math.random() < .5 ) multiplier1 = 1;
+    else multiplier1 = -1;
+
+    if( Math.random() < .5 ) multiplier2 = 1;
+    else multiplier2 = -1;
+
+    const lat = Math.random() * 90 * multiplier1;
+    const long = Math.random() * 180 * multiplier2;
+    const type = types[Math.floor(Math.random() * 3)];
+    const data = {
+      lat: lat,
+      long: long,
+      type: type,
+      text: ''
+    };
+    dataFlat.push(data);
+  }
+  return dataFlat;
+};
+
+const makeDummyDataMagnitude = () => {
+  let dataMagnitude = [];
+  
+  for(let i = 0; i < 500; i++) {
+    let multiplier1, multiplier2;
+    
+    if( Math.random() < .5 ) multiplier1 = 1;
+    else multiplier1 = -1;
+
+    if( Math.random() < .5 ) multiplier2 = 1;
+    else multiplier2 = -1;
+
+    const lat = Math.random() * 90 * multiplier1;
+    const long = Math.random() * 180 * multiplier2;
+    const type = types[Math.floor(Math.random() * 3)];
+    const data = {
+      lat: lat,
+      long: long,
+      type: type,
+      text: '',
+      magnitude: Math.random()
+    };
+    dataMagnitude.push(data);
+  }
+
+  return dataMagnitude;
+};
+
+export const dataFlat = makeDummyDataFlat();
+export const dataMagnitude = makeDummyDataMagnitude();
