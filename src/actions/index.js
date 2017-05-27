5fb64dc68e1a46f36ec49f969a9f7ae9851903d5
Abhilash Singh <daft300punk@gmail.com>
2017-05-27 12:20:54 +0530

    Thunk action for changing category

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import * as actionTypes from '../constants/actionTypes';
+import initGlobe from '../components/Globe/libs/initGlobe';
 
 export const category = (selectedCategory) => ({
   type: actionTypes.CHANGE_SELECTED_CATEGORY,
-  selectedCategory: selectedCategory
+  selectedCategory: selectedCategory.toLowerCase()
 });
 
 export const filter = (selectedFilter) => ({
@@ -12,6 +13,17 @@ export const filter = (selectedFilter) => ({
 
 export const changeSelectedFilter = (selectedFilter) => (dispatch, getState) => {
   const currentFilter = getState().selectedOptions.filter;
-  if(selectedFilter.toLowerCase() !== currentFilter)
+  if(selectedFilter.toLowerCase() !== currentFilter) {
     dispatch(filter(selectedFilter));
+    initGlobe();
+  }
 };
+
+export const changeSelectedCategory = (selectedCategory) => (dispatch, getState) => {
+  const currentCategory = getState().selectedOptions.category;
+
+  if(selectedCategory.toLowerCase() !== currentCategory) {
+    dispatch(category(selectedCategory));
+    initGlobe();
+  }
+}
