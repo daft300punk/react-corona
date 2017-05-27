903c034027e93df38f5c4607bf7b86e89f3b4295
Abhilash Singh <daft300punk@gmail.com>
2017-05-27 11:20:34 +0530

    Category and Filter constants

---
+++ src/constants/categoriesAndFilters.js
@@ -0,0 +1,30 @@
+export const categories = {
+  'activities': {
+    text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    filters: [
+      'Development',
+      'Design',
+      'Data Science'
+    ]
+  },
+  'leaderboard': [
+
+  ],
+  'submissions': [
+
+  ],
+  'tools used': [
+
+  ],
+  'tco winners': [
+
+  ]
+};
+
+export const categoryTypes = {
+  ACTIVITIES: 'activities',
+  LEADERBOARD: 'leaderboard',
+  SUBMISSIONS: 'submissions',
+  TOOLS_USED: 'tools used',
+  TCO_WINNERS: 'tco winners'
+};
