02e03be16d1e7751aad084ab5227d5af33b4f133
Abhilash Singh <daft300punk@gmail.com>
2017-05-27 12:23:23 +0530

    Update dummy constants

--- src/constants/categoriesAndFilters.js
+++ src/constants/categoriesAndFilters.js
@@ -2,29 +2,54 @@ export const categories = {
   'activities': {
     text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     filters: [
+      'All',
       'Development',
       'Design',
       'Data Science'
     ]
   },
-  'leaderboard': [
-
-  ],
-  'submissions': [
-
-  ],
-  'tools used': [
-
-  ],
-  'tco winners': [
-
-  ]
+  'leaderboard': {
+    text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    filters: [
+      'All',
+      'Development',
+      'Design',
+      'Data Science'
+    ]
+  },
+  'submissions': {
+    text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    filters: [
+      'All',
+      'Development',
+      'Design',
+      'Data Science'
+    ]
+  },
+  'tools used': {
+    text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    filters: [
+      'All',
+      'Development',
+      'Design',
+      'Data Science'
+    ]
+  },
+  'tco winners': {
+    text: '1. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    filters: [
+      'All',
+      'Development',
+      'Design',
+      'Data Science'
+    ]
+  }
 };
 
-export const categoryTypes = {
-  ACTIVITIES: 'activities',
-  LEADERBOARD: 'leaderboard',
-  SUBMISSIONS: 'submissions',
-  TOOLS_USED: 'tools used',
-  TCO_WINNERS: 'tco winners'
-};
+export const categoryTypes = [
+  'activities',
+  'leaderboard',
+  'submissions',
+  'tools used',
+  'tco winners'
+];
