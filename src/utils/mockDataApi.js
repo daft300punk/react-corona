cba9ec0b72d962d1f4aeff1c0671def9a1404035
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 15:01:37 +0530

    Mock api for producing globe data points

---
+++ src/utils/mockDataApi.js
@@ -0,0 +1,27 @@
+import {makeDummyDataFlat, makeDummyDataMagnitude} from './makeDummyData';
+
+const dataFlatAll = makeDummyDataFlat();
+const dataFlatDesign = dataFlatAll.filter((data) => {if(data.type == 'des') return data});
+const dataFlatDevelopment = dataFlatAll.filter((data) => {if(data.type == 'dev') return data});
+const dataFlatDataScience = dataFlatAll.filter((data) => {if(data.type == 'dsc') return data});
+
+const dataMagnitude = makeDummyDataMagnitude();
+
+export default function getDataApi(category, filter) {
+  const helper = (category, filter, resolve, reject) => {
+      if(category == 'all activity')
+        return resolve(JSON.stringify(dataFlatAll));
+      if(category == 'development')
+        { return resolve(JSON.stringify(dataFlatDevelopment));}
+      if(category == 'data science')
+        return resolve(JSON.stringify(dataFlatDataScience));
+      if(category == 'design')
+        return resolve(JSON.stringify(dataFlatDesign));
+      if(category == 'top winners')
+        return resolve(JSON.stringify(dataMagnitude));
+      reject('error fetching data');
+    }
+  return new Promise(function(resolve, reject) {
+    setTimeout(helper, 1000, category, filter, resolve, reject);
+  });
+}
