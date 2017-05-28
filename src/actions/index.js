09c337e86bbd9c9c23e9354c8d45ae5ad3d45b0b
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 15:01:16 +0530

    Update actions for globe data points

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import * as actionTypes from '../constants/actionTypes';
 import initGlobe from '../components/Globe/libs/initGlobe';
+import getDataApi from '../utils/mockDataApi';
 
+// Filters and categories
 export const category = (selectedCategory) => ({
   type: actionTypes.CHANGE_SELECTED_CATEGORY,
   selectedCategory: selectedCategory.toLowerCase()
@@ -15,6 +17,7 @@ export const changeSelectedFilter = (selectedFilter) => (dispatch, getState) => 
   const currentFilter = getState().selectedOptions.filter;
   if(selectedFilter.toLowerCase() !== currentFilter) {
     dispatch(filter(selectedFilter));
+    dispatch(getData());
     initGlobe();
   }
 };
@@ -24,6 +27,35 @@ export const changeSelectedCategory = (selectedCategory) => (dispatch, getState)
 
   if(selectedCategory.toLowerCase() !== currentCategory) {
     dispatch(category(selectedCategory));
+    dispatch(getData());
     initGlobe();
   }
+};
+
+
+// Data for globe points
+
+export const requestData = () =>({
+  type: actionTypes.REQUEST_DATA
+});
+
+export const receiveData = (data) => ({
+  type: actionTypes.RECEIVE_DATA,
+  data: data
+});
+
+export const error = (err) => ({
+  type: actionTypes.ERROR,
+  errorMessage: err
+})
+
+export const getData = () => (dispatch, getState) => {
+  dispatch(requestData());
+  const category = getState().selectedOptions.category;
+  const filter = getState().selectedOptions.filter;
+  getDataApi(category, filter)
+  .then(res => dispatch(receiveData(JSON.parse(res))))
+  .catch(err => {
+    dispatch(error(err));
+  });
 };
