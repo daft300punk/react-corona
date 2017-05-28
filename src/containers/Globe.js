8f216ec688de529ed4e528e08ba8cc8dfad8d72b
Abhilash Singh <daft300punk@gmail.com>
2017-05-28 15:05:29 +0530

    Globe container component

---
+++ src/containers/Globe.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Globe from '../components/Globe';
+import PropTypes from 'prop-types';
+
+const GlobeContainer = ({
+  data,
+  vizType,
+  sizeOfPoint,
+  isRequesting
+}) => {
+  return (
+
+    isRequesting ? <div/> : (
+      <Globe
+        data={data}
+        vizType={vizType}
+        sizeOfPoint={sizeOfPoint}
+      />
+    )
+
+  );
+};
+
+const mapStateToProps = (state) => ({
+  data: state.globeData.data,
+  vizType: state.globeData.vizType,
+  sizeOfPoint: state.globeData.sizeOfPoint,
+  isRequesting: state.globeData.isRequesting
+});
+
+
+GlobeContainer.propTypes = {
+  data: PropTypes.object,
+  vizType: PropTypes.string,
+  sizeOfPoint: PropTypes.number,
+  isRequesting: PropTypes.bool
+};
+
+export default connect(
+  mapStateToProps
+)(GlobeContainer);
